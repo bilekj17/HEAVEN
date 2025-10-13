f306364005697bc6b3a7b6e7faec4b7479e7e46d
honza <bilekj@gasos-ro.cz>
2025-10-13 18:30:58 +0200

    gg

---
+++ core/src/main/java/io/github/Heaven/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package io.github.Heaven;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Moje hra";
+        config.width = 800;
+        config.height = 600;
+        new LwjglApplication(new Main(), config);
+    }
+}
